940bce1886d71836326e16f9afecc393a0ba19cc
raitucarp <ribhararnus.pracutiar@gmail.com>
2015-06-13 21:54:35 +0700

    delete unnecessary comment

--- LocaleValues.go
+++ LocaleValues.go
@@ -1,38 +1,8 @@
 package agozon
 
+// Locale Information
 type LocaleSearchIndex struct {
 	BrowseNode           int64    `json:"BrowseNode"`
 	SortValues           []string `json:"SortValues"`
 	ItemSearchParameters []string `json:"ItemSearchParameters"`
 }
-
-var y = LocaleUS
-
-/*
-
-var LocaleBR = map[string]LocaleSearchIndex{}
-var LocaleCA = map[string]LocaleSearchIndex{}
-var LocaleCN = map[string]LocaleSearchIndex{}
-var LocaleDE = map[string]LocaleSearchIndex{}
-var LocaleES = map[string]LocaleSearchIndex{}
-var LocaleFR = map[string]LocaleSearchIndex{}
-var LocaleIN = map[string]LocaleSearchIndex{}
-var LocaleIT = map[string]LocaleSearchIndex{}
-var LocaleJP = map[string]LocaleSearchIndex{}
-var LocaleUK = map[string]LocaleSearchIndex{}
-
-var LocaleReference = map[string]map[string]LocaleSearchIndex{
-	"BR": LocaleBR,
-	"CA": LocaleCA,
-	"CN": LocaleCN,
-	"DE": LocaleDE,
-	"ES": LocaleES,
-	"FR": LocaleFR,
-	"IN": LocaleIN,
-	"IT": LocaleIT,
-	"JP": LocaleJP,
-	"UK": LocaleUK,
-	"US": LocaleUS,
-}
-*/
-//var Marketplaces = []Marketplace{}
